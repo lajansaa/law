ac8f8083a2155f1bfc5914094c2a61bebac1612d
Isa <lajansa@hotmail.com>
2018-07-25 22:59:44 +0800

    added dashboard and show submitted response

---
+++ src/components/SubmittedForm.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-layout row justify-center>
+    <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+      <v-btn slot="activator" small color="primary" dark>{{ buttonLabel }}</v-btn>
+      <v-card>
+
+        <v-toolbar dark color="primary">
+          <v-btn icon dark @click.native="dialog = false">
+            <v-icon>close</v-icon>
+          </v-btn>
+          <v-toolbar-title>Settings</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-items>
+            <v-btn dark flat @click.native="dialog = false">Save</v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
+
+        <v-container fluid>
+          <h1>Submitted Case</h1>
+          <h3 class="my-3" style="textDecoration: underline;">Client Details</h3>
+          <p class="ml-3" v-for="(value, key) in data.client"><b>{{prettify(key)}}</b>: {{value}}</p>
+
+          <h3 class="my-3" style="textDecoration: underline;">Spouse Details</h3>
+          <p class="ml-3" v-for="(value, key) in data.spouse"><b>{{prettify(key)}}</b>: {{value}}</p>
+
+          <h3 class="my-3" style="textDecoration: underline;">Marriage Details</h3>
+          <p class="ml-3" v-for="(value, key) in data.marriage"><b>{{prettify(key)}}</b>: {{value}}</p>
+
+          <h3 class="my-3" style="textDecoration: underline;">Children Details</h3>
+          <p class="ml-3" v-for="(value, key) in data.children"><b>{{prettify(key)}}</b>: {{value}}</p>
+        </v-container>
+
+      </v-card>
+    </v-dialog>
+  </v-layout>
+</template>
+
+<script>
+export default {
+    name: 'SubmittedForm',
+    props: [ 'buttonLabel', 'data' ],
+    data () {
+      return {
+        dialog: false
+      }
+    },
+    methods: {
+      prettify (word) {
+        let converted = word.replace(/(?:^|\.?)([A-Z])/g, (p1, p2) => {return " " + p2})
+        converted = converted.charAt(0).toUpperCase() + converted.substr(1)
+        return converted
+      }
+    }
+}
+</script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,15 +1,57 @@
 <template>
   <div class="dashboard">
     <h1>Dashboard Page</h1>
+    <v-data-table
+      :headers="headers"
+      :items="cases"
+      class="elevation-1"
+    >
+      <template slot="items" slot-scope="props">
+        <td class="text-xs-center">{{ props.item.client.firstName }} {{ props.item.client.lastName }}</td>
+        <td class="text-xs-center">{{ props.item.client.contactNumber }}</td>
+        <td class="text-xs-center">{{ props.item.client.email }}</td>
+        <td class="text-xs-center">{{ props.item.createdAt.slice(0, -18) }}</td>
+        <td class="justify-center layout px-0">
+          <SubmittedForm
+            buttonLabel="Submitted"
+            :data= "props.item"
+          />
+          <div>
+            <v-btn small color="primary" @click="generateContract(props.item)">Contract</v-btn>
+          </div>
+        </td>
+      </template>
+    </v-data-table>
   </div>
 </template>
 
 <script>
+import db from '../firebaseInit'
+import SubmittedForm from '../components/SubmittedForm'
 
 export default {
   name: 'dashboard',
   components: {
-      
-  }
+      SubmittedForm
+  },
+  data: () => ({
+      dialog: false,
+      headers: [
+        { text: 'Client Name', value: 'clientName', align: 'center' },
+        { text: 'Contact Number', value: 'contactNumber', align: 'center' },
+        { text: 'Email', value: 'email', align: 'center' },
+        { text: 'Submitted Date (SGT)', value: 'submittedDate', align: 'center' },
+        { text: 'Actions', value: 'actions', sortable: false, align: 'center' }
+      ],
+      cases: []
+    }),
+    created () {
+      const casesRef = db.database().ref('cases')
+      casesRef.orderByKey().on("value", (data) => {
+          const dataObj = data.val()
+          const dataKey = Object.keys(dataObj)
+          this.cases = dataKey.map((e) => dataObj[e])
+      })
+    }
 }
 </script>
